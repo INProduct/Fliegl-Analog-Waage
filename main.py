3311222fd93c911b0465bd8625f00f7e4c138a3d
alex <a.jurcenko@inproduct.de>
2021-09-12 11:35:26 +0200

    worked on web-sites

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from app import App
 import web_server
 
 a = App()
+web_server.set_app(a)
 web_server.startWebServer()
--- web_server.py
+++ web_server.py
@@ -1,28 +1,85 @@
 from microWebSrv import MicroWebSrv
 
+
+gapp = None
+
+def set_app(app):
+    global gapp
+    gapp = app
+
 @MicroWebSrv.route('/notfound')
 def _httpHandler_notfound(httpClient, httpResponse):
     httpResponse.WriteResponsePyHTMLFile('www/notfound.pyhtml', vars={'title':'Not Found'})
 
+
+
 @MicroWebSrv.route('/index')
 def _httpHandler_index(httpClient, httpResponse):
-    httpResponse.WriteResponsePyHTMLFile('www/index.pyhtml')
+    httpResponse.WriteResponsePyHTMLFile('www/index.pyhtml', vars={
+        'weight': str(gapp.waage.get_tared_weight()),
+        'portion': str(gapp.get_portion()),
+    })
+
+@MicroWebSrv.route('/index/<command>')
+def _httpHandler_index(httpClient, httpResponse, routeArgs):
+    print(routeArgs)
+    if 'start' in routeArgs['command']:
+        gapp.start()
+    elif 'stopor' in routeArgs['command']:
+        gapp.start_one_portion()
+    elif 'stop' in routeArgs['command']:
+        gapp.stop()
+    httpResponse.WriteResponsePyHTMLFile('www/index.pyhtml', vars={
+        'weight': str(gapp.waage.get_tared_weight()),
+        'portion': str(gapp.get_portion()),
+    })
+
+@MicroWebSrv.route('/waage')
+def _httpHandler_index(httpClient, httpResponse):
+    httpResponse.WriteResponsePyHTMLFile('www/waage.pyhtml', vars={
+        'weight': str(gapp.waage.get_tared_weight()),
+        'portion': str(gapp.get_portion()),
+    })
+
+@MicroWebSrv.route('/waage/<command>')
+def _httpHandler_index(httpClient, httpResponse, routeArgs):
+    print(routeArgs)
+    if 'zeropoint' in routeArgs['command']:
+        gapp.waage.set_zeropoint()
+    elif 'calibrate' in routeArgs['command']:
+        gapp.waage.set_calibration(65)
+    elif 'tara' in routeArgs['command']:
+        gapp.waage.set_tara()
+    httpResponse.WriteResponsePyHTMLFile('www/waage.pyhtml', vars={
+        'weight': str(gapp.waage.get_tared_weight()),
+        'portion': str(gapp.get_portion()),
+    })
+
 
 @MicroWebSrv.route('/info')
 def _httpHandler_test(httpClient, httpResponse):
-    httpResponse.WriteResponsePyHTMLFile('www/info.pyhtml', vars={'title':'Info'})
+    httpResponse.WriteResponsePyHTMLFile('www/info.pyhtml', vars={'title': str(gapp.get_portion())})
+
 
 @MicroWebSrv.route('/settings')
 def _httpHandler_test(httpClient, httpResponse):
     httpResponse.WriteResponsePyHTMLFile('www/settings.pyhtml')
 
+
 @MicroWebSrv.route('/bootstrap.min.css')
 def _httpHandler_getbootstrap(httpClient, httpResponse):
     httpResponse.WriteResponseFile('www/css/bootstrap.min.css', contentType='text/css')
 
+
 @MicroWebSrv.route('/bootstrap.min.js')
 def _httpHandler_getbootstrap(httpClient, httpResponse):
     httpResponse.WriteResponseFile('www/js/bootstrap.min.js', contentType='text/javascript')
+
+@MicroWebSrv.route('/start.pyhtml')
+def _httpHandler_start(httpClient, httpResponse):
+    gapp.start()
+    httpResponse.WriteResponseOK()
+
 
 def startWebServer():
     mws = MicroWebSrv()
